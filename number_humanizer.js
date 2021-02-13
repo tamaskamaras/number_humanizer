6415827761ba567dcf6e175bced9e9a96047cc31
Tamás Kamarás <kamaras.tamas.viktor@gmail.com>
2021-02-13 21:17:49 +0100

    implement 3 digit

--- number_humanizer.js
+++ number_humanizer.js
@@ -6,8 +6,7 @@ const toTens = (number) => {
     case 1:
       return PLACE_VALUES.ones[number];
     case 2:
-      let first, second;
-      [firstDigit, secondDigit] = number.split('');
+      let [firstDigit, secondDigit] = number.split('');
       let result = PLACE_VALUES.tens[firstDigit];
 
       if (firstDigit === '1') {
@@ -18,10 +17,24 @@ const toTens = (number) => {
       }
       return result;
     default:
-      console.error('length of argument must be between 1..2')
+      console.error('length of argument must be between 1..2');
   }
 };
 
 exports.humanizedNumberFrom = (inputNumber) => {
-  return toTens(inputNumber.toString().slice(-2));
+  let baseNumber = inputNumber.toString();
+  let result = toTens(baseNumber.slice(-2));
+  baseNumber = baseNumber.slice(0, -2)
+
+  for (const stepName of STEPS) {
+    if (baseNumber.length) {
+      current = `${toTens(baseNumber.slice(-2))} ${stepName}`
+      result = result ? `${current} and ${result}` : current
+      baseNumber = baseNumber.slice(0, -2)
+    } else {
+      break;
+    }
+  }
+
+  return result;
 }
